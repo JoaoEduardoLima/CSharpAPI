8f369c021fa93501a6dccd48d05581306eebc50f
joaoe <joaoeduardo.lima@hotmail.com>
2023-07-13 23:55:09 -0300

    Adiciona classe generica de retorno dos serviços

---
+++ Model.Application/Services/ResultService.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation.Results;
+
+namespace Model.Application.Services
+{
+    public class ResultService
+    {
+        public bool IsSucess { get; set; }
+        public string Message { get; set; }
+        public ICollection<ErrorValidation> Errors { get; set; }
+
+        public static ResultService RequestError(string message, ValidationResult validationResult)
+        {
+            return new ResultService
+            {
+                IsSucess = false,
+                Message = message,
+                Errors = validationResult.Errors.Select(x => new ErrorValidation { Field = x.PropertyName, Message = x.ErrorMessage }).ToList()
+            };
+        }
+    }
+    public class ResultService<T> : ResultService
+    {
+        public T Data { get; set; }
+    }
+}
